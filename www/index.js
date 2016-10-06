188892923d2583c0f24e7da04ace21e11156b77d
weaverwi <willweaver@auburnmechanical.com>
2016-10-06 14:53:14 -0700

    Update index.js

--- www/index.js
+++ www/index.js
@@ -15,7 +15,7 @@
 // DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////////////////
-var defaultUrn = '<replace with your encoded urn>';
+var defaultUrn = dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE2LTEwLTA2LTIxLTM2LTA5LXRwOWRwdmhhem9scWhkcjNrYXFwMmJnYnZsajAvUm9ib3RBcm0uZHdmeA;
 
 $(document).ready(function () {
     var tokenurl = 'http://' + window.location.host + '/api/token';
